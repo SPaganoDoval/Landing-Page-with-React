0f01f14eca1aefa4ad3d8ef8c331434ed69a40e6
SPaganoDoval <dovalsonia@gmail.com>
2022-11-28 20:49:23 +0000

    01 inicio proyecto Navbar y Jumbotron iniciados

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function Jumbotron (props) {
+    return (<p>Jumbotron</p>);
+    
+};
+export default Jumbotron;
---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function Navbar (props) {
+    return ( 
+        <div>
+                <p>Navbar</p>
+                <h1>texto</h1>
+        </div>
+    );
+    
+};
+export default Navbar;
