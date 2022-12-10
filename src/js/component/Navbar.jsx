5f9524ae63f4d9712c704005af8babc983536f07
SPaganoDoval <dovalsonia@gmail.com>
2022-12-10 17:52:12 +0000

    navbar nombres

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -11,13 +11,13 @@ function Navbar (props) {
                      <a class="nav-link home" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                     <a class="nav-link" href="#">Link</a>
+                     <a class="nav-link" href="#">About</a>
                 </li>
                 <li class="nav-item">
-                     <a class="nav-link" href="#">Link</a>
+                     <a class="nav-link" href="#">services</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link disabled">Contact</a>
                 </li>
             </ul>
         </div>
